2e7573992f4b0eb4b4c148eea6aa4a537d1bf185
Stefan Mejlgaard <connesy@gmail.com>
2021-04-26 18:01:20 +0200

    Turn Log and _point into actual classes. Rename `mainMenu` class to `MainMenu`

--- classes.js
+++ classes.js
@@ -1,27 +1,31 @@
-function _point(x, y) {
-  this.x = x;
-  this.y = y;
+class _point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 function Point(x, y) {
   return new _point(x, y);
 }
 
-function Log() {
-  this._log = (message, level, style) => {
-    console.log(`%c${level}%c - ${message}`, style, "")
-  };
-  this.DEBUG = (message) => {
-    this._log(message, "DEBUG", "color: blue")
-  };
-  this.INFO = (message) => {
-    this._log(message, "INFO", "")
-  };
-  this.WARNING = (message) => {
-    this._log(message, "WARNING", "color: yellow")
-  };
-  this.ERROR = (message) => {
-    this._log(message, "ERROR", "color: red; font-weight: bold")
-  };
+class Log {
+  constructor() {
+    this._log = ( message, level, style ) => {
+      console.log( `%c${level}%c - ${message}`, style, "" );
+    };
+    this.DEBUG = ( message ) => {
+      this._log( message, "DEBUG", "color: blue" );
+    };
+    this.INFO = ( message ) => {
+      this._log( message, "INFO", "" );
+    };
+    this.WARNING = ( message ) => {
+      this._log( message, "WARNING", "color: yellow" );
+    };
+    this.ERROR = ( message ) => {
+      this._log( message, "ERROR", "color: red; font-weight: bold" );
+    };
+  }
 }
 
 
@@ -30,13 +34,9 @@ function loadImage(scene, key, imageName) {
   scene.load.image(key, cartographyPath + imageName)
 }
 
-class mainMenu extends Phaser.Scene {
+class MainMenu extends Phaser.Scene {
   constructor () {
-      super('mainMenu');
-  }
-
-  init() {
-      // Used to prepare data
+      super('MainMenu');
   }
 
   preload() {
@@ -47,7 +47,6 @@ class mainMenu extends Phaser.Scene {
 
   create(data) {
     this.add.image(0,0,'backgroundPaper')
-
     this.add.text(10,10,'Deeper and deeper into the forest')
   }
 
