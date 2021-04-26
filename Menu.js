d03f36994dfe4b3d423dfe681a293c01c5d01b89
Stefan Mejlgaard <connesy@gmail.com>
2021-04-26 18:38:57 +0200

    Move MainMenu class to Menu.js

--- Menu.js
+++ Menu.js
@@ -1,24 +1,21 @@
-class Menu extends Phaser.Scene {
-
-  constructor () {
-      super('Menu');
+class MainMenu extends Phaser.Scene {
+    constructor () {
+        super('MainMenu');
+    }
+  
+    preload() {
+      loadImage(this, 'player', 'elementDiamond.png');
+      loadImage(this, 'caravan', 'arrowSmall.png');
+      this.load.image('backgroundPaper', 'assets/cartographypack/Textures/parchmentBasic.png')
+    }
+  
+    create(data) {
+      this.add.image(0,0,'backgroundPaper')
+      this.add.text(10,10,'Deeper and deeper into the forest')
+    }
+  
+    update(time, delta) {
+        // Used to update your game. This function runs constantly
+    }
+    
   }
-
-  init() {
-      // Used to prepare data
-  }
-
-  preload() {
-      // Used for preloading assets into your scene, such as
-      // • images
-      // • sounds
-  }
-
-  create(data) {
-      // Used to add objects to your game
-  }
-
-  update(time, delta) {
-      // Used to update your game. This function runs constantly
-  }
-}
--- classes.js
+++ classes.js
@@ -30,25 +30,3 @@ function loadImage(scene, key, imageName) {
   let cartographyPath = 'assets/cartographypack/PNG/Retina/'
   scene.load.image(key, cartographyPath + imageName)
 }
-
-class MainMenu extends Phaser.Scene {
-  constructor () {
-      super('MainMenu');
-  }
-
-  preload() {
-    loadImage(this, 'player', 'elementDiamond.png');
-    loadImage(this, 'caravan', 'arrowSmall.png');
-    this.load.image('backgroundPaper', 'assets/cartographypack/Textures/parchmentBasic.png')
-  }
-
-  create(data) {
-    this.add.image(0,0,'backgroundPaper')
-    this.add.text(10,10,'Deeper and deeper into the forest')
-  }
-
-  update(time, delta) {
-      // Used to update your game. This function runs constantly
-  }
-  
-}
